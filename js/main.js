f4744f34191622b46a7bc6ac080e4d1d6bcf2477
ReDMallard <marco@titori.it>
2023-11-03 01:20:17 +0100

    add feat brightness miniatures carousel 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const arrayImmagini = [
-    "https://picsum.photos/id/237/900/400", 
+    "https://picsum.photos/id/237/900/400",
     "https://picsum.photos/id/238/900/400",
     "https://picsum.photos/id/239/900/400",
     "https://picsum.photos/id/240/900/400",
@@ -12,7 +12,7 @@ let elementImmagini = "";
 for (let i = 0; i < arrayImmagini.length; i++) {
     const linkImmagine = arrayImmagini[i];
     elementImmagini += `<img class="wait wideFull" src="${linkImmagine}">`;
-    
+
 }
 document.getElementById("carousel").innerHTML = elementImmagini;
 
@@ -24,32 +24,32 @@ images[currentImg].classList.add("active");
 
 
 document.getElementById("nextBtn").addEventListener("click", function () {
-    
+
     if (currentImg < images.length - 1) {
         images[currentImg].classList.remove("active");
         images[currentImg].classList.add("wait");
         currentImg++;
         images[currentImg].classList.add("active");
         images[currentImg].classList.remove("wait");
-    } else{
+    } else {
         images[currentImg].classList.remove("active");
         images[currentImg].classList.add("wait");
         currentImg = (0);
         images[currentImg].classList.add("active");
         images[currentImg].classList.remove("wait");
-        
+
     }
 });
 
 document.getElementById("prevBtn").addEventListener("click", function () {
-    
+
     if (currentImg > 0) {
         images[currentImg].classList.remove("active");
         images[currentImg].classList.add("wait");
         currentImg--;
         images[currentImg].classList.add("active");
         images[currentImg].classList.remove("wait");
-    }else {
+    } else {
         images[currentImg].classList.remove("active");
         images[currentImg].classList.add("wait");
         currentImg = (images.length - 1);
@@ -72,48 +72,67 @@ let elementMiniImmagini = "";
 for (let i = 0; i < array2Immagini.length; i++) {
     const link2Immagine = array2Immagini[i];
     element2Immagini += `<img id="midWidth" class="wait wideMid" src="${link2Immagine}">`;
-    elementMiniImmagini += `<img class="widemini" src="${link2Immagine}">`;
-    
+    elementMiniImmagini += `<img id="miniWidth" class="widemini miniWait" src="${link2Immagine}">`;
+
 }
 document.getElementById("carouselMin").innerHTML += element2Immagini;
 document.getElementById("miniSide").innerHTML = elementMiniImmagini;
 
 let current2Img = 0;
 const images2 = document.querySelectorAll("#midWidth");
+const miniature = document.querySelectorAll("#miniWidth");
 images2[current2Img].classList.remove("wait");
 images2[current2Img].classList.add("active");
+miniature[current2Img].classList.remove("miniWait");
+miniature[current2Img].classList.add("miniActive");
 
 document.getElementById("nextMinBtn").addEventListener("click", function () {
-    
+
     if (current2Img < images2.length - 1) {
         images2[current2Img].classList.remove("active");
         images2[current2Img].classList.add("wait");
+        miniature[current2Img].classList.add("miniWait");
+        miniature[current2Img].classList.remove("miniActive");
         current2Img++;
         images2[current2Img].classList.add("active");
         images2[current2Img].classList.remove("wait");
-    } else{
+        miniature[current2Img].classList.remove("miniWait");
+        miniature[current2Img].classList.add("miniActive");
+    } else {
         images2[current2Img].classList.remove("active");
         images2[current2Img].classList.add("wait");
+        miniature[current2Img].classList.add("miniWait");
+        miniature[current2Img].classList.remove("miniActive");
         current2Img = (0);
         images2[current2Img].classList.add("active");
         images2[current2Img].classList.remove("wait");
-        
+        miniature[current2Img].classList.remove("miniWait");
+        miniature[current2Img].classList.add("miniActive");
+
     }
 });
 
 document.getElementById("prevMinBtn").addEventListener("click", function () {
-    
+
     if (current2Img > 0) {
         images2[current2Img].classList.remove("active");
         images2[current2Img].classList.add("wait");
+        miniature[current2Img].classList.add("miniWait");
+        miniature[current2Img].classList.remove("miniActive");
         current2Img--;
         images2[current2Img].classList.add("active");
         images2[current2Img].classList.remove("wait");
-    }else {
+        miniature[current2Img].classList.remove("miniWait");
+        miniature[current2Img].classList.add("miniActive");
+    } else {
         images2[current2Img].classList.remove("active");
         images2[current2Img].classList.add("wait");
+        miniature[current2Img].classList.add("miniWait");
+        miniature[current2Img].classList.remove("miniActive");
         current2Img = (images2.length - 1);
         images2[current2Img].classList.add("active");
         images2[current2Img].classList.remove("wait");
+        miniature[current2Img].classList.remove("miniWait");
+        miniature[current2Img].classList.add("miniActive");
     }
 });
